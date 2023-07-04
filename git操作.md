208afcc927e666c913079b7a8658fd512d10c24f
xxx <xxx@xxx>
2023-07-04 09:43:39 +0800

    7-4提交

---
+++ git操作.md
@@ -0,0 +1,42 @@
+# git 操作
+
+ 查看远程仓库
+ > git remote
+
+查看远程仓库详细信息
+ > git remote -v
+ 
+
+添加远程仓库
+
+ > git remote add origin <url>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/itheima/reggie/service/CategoryService.java
@@ -0,0 +1,17 @@
+package com.itheima.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.itheima.reggie.entity.Category;
+import org.springframework.context.annotation.Lazy;
+
+/**
+ * @author LJM
+ * @create 2022/4/16
+ */
+
+
+public interface CategoryService extends IService<Category> {
+
+    //定义自己需要的方法
+    void remove(Long id);
+}
