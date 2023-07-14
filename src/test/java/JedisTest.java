8a10ac6f74422f79defd9e83d910346cdc7fad3e
cwRising <3185087246@qq.com>
2023-07-14 13:22:29 +0800

    增加Redis说明，增加redis测试类

---
+++ src/test/java/JedisTest.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+import redis.clients.jedis.Jedis;
+/***
+* @Description: 这是一个测试redis的工具
+* @Param:
+* @return:
+* @Author: ChangWei Fu
+* @DATE: 2023-07-14   13:19
+*/
+
+public class JedisTest {
+    @Test
+    public void tetsRedis(){
+        Jedis jedis = new Jedis("localhost",6379);
+
+        // 输入密码
+        jedis.auth("123456");
+
+        jedis.set("username","xiaoming");
+
+        String value=jedis.get("username");
+        System.out.println(value);
+
+        jedis.del("username");
+
+        jedis.close();
+    }
+}
+
+// P152
