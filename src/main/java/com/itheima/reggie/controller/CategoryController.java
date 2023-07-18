bc3f1afda30a102166b97a276a3ff535904c4d1d
cwRising <3185087246@qq.com>
2023-07-18 11:59:44 +0800

    增加注释：CatedoryCintroller

--- src/main/java/com/itheima/reggie/controller/CategoryController.java
+++ src/main/java/com/itheima/reggie/controller/CategoryController.java
@@ -11,95 +11,61 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-/**
- * @author LJM
- * @create 2022/4/16
- * 分类管理
- */
+// 分类管理
+@Slf4j
+// 将一个类声明为控制器类，并自动将方法的返回值转换为 JSON 格式的响应数据。
 @RestController
 @RequestMapping("/category")
-@Slf4j
 public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
 
-    /**
-     * 新增套餐分类
-     * @param category
-     * @return
-     */
+    // 新增套餐分类
     @PostMapping
-    public R<String> save(@RequestBody Category category){
-        log.info("{category}" ,category);
+    public R<String> save(@RequestBody Category category) {
+        log.info("{category}", category);
         categoryService.save(category);
         return R.success("新增分类成功");
     }
 
-
-    /**
-     * 分页查询
-     * @param page
-     * @param pageSize
-     * @return
-     */
+    // 分页查询
     @GetMapping("/page")
-    public R<Page> page(int page,int pageSize){
-
-        //创建一个分页构造器
-        Page<Category> categoryPage = new Page<>(page,pageSize);
-        //创建一个条件构造器  用来排序用的  注意这个条件构造器一定要使用泛型，否则使用条件查询这个方法的时候会报错
-        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper();
-        //添加排序条件 ，根据sort字段进行排序
+    public R<Page> page(int page, int pageSize) {
+        // 创建一个分页构造器
+        // 这两个参数表示要查询的页码和每页的数据量。这些参数通过 URL 查询参数传递，如：/users?page=1&size=10。
+        Page<Category> categoryPage = new Page<>(page, pageSize);
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+        //添加排序条件 ，根据sort字段进行升序
         queryWrapper.orderByAsc(Category::getSort);
-        categoryService.page(categoryPage,queryWrapper);
+        categoryService.page(categoryPage, queryWrapper);
         return R.success(categoryPage);
     }
 
-    /**
-     * 根据id来删除分类的数据
-     * @param id
-     * @return
-     */
+    // 根据id来删除分类的数据
     @DeleteMapping
-    public R<String> delete(@RequestParam("ids") Long id){ //注意这里前端传过来的数据是ids
+    public R<String> delete(@RequestParam("ids") Long id) { //注意这里前端传过来的数据是ids
         categoryService.remove(id);
         return R.success("分类信息删除成功");
     }
 
-
-    /**
-     * 根据id修改分类
-     * @param category
-     * @return
-     */
+    // 根据id修改分类
     @PutMapping
-    public R<String> update(@RequestBody Category category){
+    public R<String> update(@RequestBody Category category) {
         categoryService.updateById(category);
         return R.success("修改分类信息成功");
     }
 
-
-    /**
-     * 根据条件查询分类数据
-     * @param category
-     * @return
-     */
+    // 根据条件查询分类数据
     @GetMapping("/list")
     //这个接口接收到参数其实就是一个前端传过来的type,这里之所以使用Category这个类来接受前端的数据，是为了以后方便
     //因为这个Category类里面包含了type这个数据,返回的数据多了，你自己用啥取啥就行
-    private R<List<Category>> list(Category category){
-        //条件构造器
-        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper();
-        //添加查询条件
-        queryWrapper.eq(category.getType() != null,Category::getType,category.getType());
-        //添加排序条件  使用两个排序条件,如果sort相同的情况下就使用更新时间进行排序
+    //@RestController 注解已经包含了 @ResponseBody 注解的功能，因此在使用 @RestController 注解的控制器方法中，不需要再单独添加 @ResponseBody 注解。
+    private R<List<Category>> list(Category category) {
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(category.getType() != null, Category::getType, category.getType());
         queryWrapper.orderByAsc(Category::getSort).orderByDesc(Category::getUpdateTime);
         List<Category> list = categoryService.list(queryWrapper);
-
         return R.success(list);
     }
-
-
-
 }
